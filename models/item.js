60d09d9571ba2531026a7b8005ee511a3b0719f5
fish <fish404hsif@gmail.com>
2023-01-18 22:52:07 +0800

    :construction: add movie class(extends from Item class)

--- models/item.js
+++ models/item.js
@@ -1,4 +1,7 @@
 const config = require("../utils/config");
+const got = require("got");
+const {JSDOM} = require("jsdom");
+const {DB_PROPERTIES} = require('../util');
 
 const notionDbMap = new Map([
       ['movie', config.MOVIE_DB_ID]
@@ -22,8 +25,41 @@ class Item
     getNotionDbId() {
         return this._notionDbId;
     }
+
+    async _setItemLinkDom() {
+        const response = await got(this._link);
+        this._dom = new JSDOM(response.body);
+    }
+}
+
+class Movie extends Item 
+{
+    // @todo: rewrite return type
+    async setInfo() {
+        await this._setItemLinkDom();
+        // 对应条目括号内的年份(非上映日期)
+        this._year = this._dom.window.document.querySelector('#content h1 .year').textContent.slice(1, -1);
+        this._directors = this._dom.window.document.querySelector('#info .attrs').textContent;
+        this._actors = [...this._dom.window.document.querySelectorAll('#info .actor .attrs a')].slice(0, 5).map(i => i.textContent).join(' / ');
+        this._genre = [...this._dom.window.document.querySelectorAll('#info [property="v:genre"]')].map(i => i.textContent); // array
+        this._imdbNo = [...this._dom.window.document.querySelectorAll('#info span.pl')].filter(i => i.textContent.startsWith('IMDb'));
+        if (this._imdbNo.length) {
+          this._imdbLink = 'https://www.imdb.com/title/' + this._imdbNo[0].nextSibling.textContent.trim();
+        }
+    }
+
+    async getInfo() {
+        return {
+            [DB_PROPERTIES.YEAR]: this._year
+            , [DB_PROPERTIES.DIRECTORS]: this._directors
+            , [DB_PROPERTIES.ACTORS]: this._actors
+            , [DB_PROPERTIES.GENRE]: this._genre
+            , [DB_PROPERTIES.IMDB_LINK]: this._imdbLink
+        }
+    }
 }
 
 module.exports = {
     Item
+    , Movie
 }
