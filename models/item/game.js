8dd7ea16ce7bd8eb99d8935bd597a42eb879ab04
fish <fish404hsif@gmail.com>
2023-01-19 22:29:32 +0800

    :hammer: add game class (extends item class)

---
+++ models/item/game.js
@@ -0,0 +1,34 @@
+const {Item} = require("../item")
+const {DB_PROPERTIES} = require("../../util");
+const dayjs = require("dayjs");
+
+class Game extends Item 
+{
+    // @todo: rewrite return type
+    async setInfo() {
+        await this._setItemLinkDom();
+        const gameInfo = this._dom.window.document.querySelector('#content .game-attr');
+        const dts = [...gameInfo.querySelectorAll('dt')].filter(i => i.textContent.startsWith('类型') || i.textContent.startsWith('发行日期'));
+        if (dts.length) {
+            dts.forEach(dt => {
+                if (dt.textContent.startsWith('类型')) {
+                    this._genre = [...dt.nextElementSibling.querySelectorAll('a')].map(a => a.textContent.trim()); //array
+                } else if (dt.textContent.startsWith('发行日期')) {
+                    let date = dt.nextElementSibling.textContent.trim();
+                    this._releaseDate = dayjs(date).format('YYYY-MM-DD');
+                }
+            })
+        }
+    }
+
+    async getInfo() {
+        return {
+            [DB_PROPERTIES.GENRE]: this._genre
+            , [DB_PROPERTIES.RELEASE_DATE]: this._releaseDate
+        }
+    }
+}
+
+module.exports = {
+    Game
+}
